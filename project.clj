d9ce8d8ef9cba1cf61cb53777a0b69368df4fb49
ertugrulcetin <ertu.ctn@gmail.com>
2021-05-08 23:20:15 +0300

    update dev env

--- project.clj
+++ project.clj
@@ -6,20 +6,19 @@
   :url "https://github.com/ertugrulcetin/re-frame-flow"
 
   :license {:name "MIT License"
-            :url  "https://opensource.org/licenses/MIT"}
+            :url "https://opensource.org/licenses/MIT"}
 
   :dependencies [[thheller/shadow-cljs "2.12.5"]]
 
   :min-lein-version "2.9.0"
 
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+  :clean-targets ^{:protect false} ["dev/resources/public/js/compiled" "target"]
 
   :source-paths ["src"]
-
-  :resource-paths ["resources" "target/cljsbuild"]
 
   :aliases {"watch" ["with-profile" "dev" "run" "-m" "shadow.cljs.devtools.cli" "watch" "app"]}
 
   :profiles {:dev {:dependencies [[reagent "1.0.0"]
                                   [re-frame "1.2.0"]]
-                   :source-paths ["dev"]}})
+                   :source-paths ["dev"]
+                   :resource-paths ["dev/resources"]}})
