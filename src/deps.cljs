343e00f897855e58964792348a0d2074e6d1e7ba
ertugrulcetin <ertu.ctn@gmail.com>
2021-05-09 17:45:45 +0300

    Add missing deps.cljs

---
+++ src/deps.cljs
@@ -0,0 +1,7 @@
+{:npm-dev-deps {"shadow-cljs" "2.12.5"
+                "karma" "5.2.3"
+                "karma-chrome-launcher" "3.1.0"
+                "karma-cljs-test" "0.1.0"
+                "karma-junit-reporter" "2.0.1"}
+ :npm-deps {"dagre" "^0.8.5"
+            "react-flow-renderer" "^9.5.4"}}
